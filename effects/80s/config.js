d3e492807fca30092d33afbae4913342c7ccfbce
Ihar Tumašyk <ihar.tumashyk@banuba.com>
2024-02-09 13:39:24 +0300

    Remove files from lfs

--- effects/80s/config.js
+++
@@ -1,4 +0,0 @@
-Background = require('bnb_js/background');
-
-Background.texture("videos/room.mp4");
-Background.contentMode("fill");
--- effects/TouchUp/modules/face-morph/draw.frag
+++
@@ -1,9 +0,0 @@
-#include <bnb/glsl.frag>
-
-BNB_IN(0)
-vec2 var_c;
-
-void main()
-{
-    bnb_FragColor = vec4(var_c, 0., 0.);
-}
