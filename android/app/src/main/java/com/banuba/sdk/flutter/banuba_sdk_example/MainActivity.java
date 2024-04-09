d30045610b553f2e302c22a6f292c000193fbc8d
Ihar Tumašyk <ihar.tumashyk@banuba.com>
2024-04-09 10:25:16 +0300

    replace FlutterActivity with FlutterFragmentActivity

--- android/app/src/main/java/com/banuba/sdk/flutter/banuba_sdk_example/MainActivity.java
+++ android/app/src/main/java/com/banuba/sdk/flutter/banuba_sdk_example/MainActivity.java
@@ -1,6 +1,6 @@
 package com.banuba.sdk.flutter.banuba_sdk_example;
 
-import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterFragmentActivity;
 
-public class MainActivity extends FlutterActivity {
+public class MainActivity extends FlutterFragmentActivity {
 }
